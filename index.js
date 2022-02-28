ba00f7d8e72295a6ca74c8ae89334b7bc1b42966
AlexMinez <alex_minihan@outlook.com>
2022-02-28 19:56:59 +1100

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(scuberGreetingForFeet){  
+  if (scuberGreetingForFeet <=400) {
+  return 'This one is on me!';
+ } else if (scuberGreetingForFeet <= 2001){
+  return 'I will gladly take your thirty bucks.';
+} else if (scuberGreetingForFeet >=2501){
+   return 'No can do.';
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(ternaryCheckCity){
+  return ternaryCheckCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+function switchOnCharmFromTip(switchOnCharmFromTip){
+  switch(switchOnCharmFromTip){
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default: 'thanks for everything'
+      return 'Bye.'
+      break;    
+  }
 }
